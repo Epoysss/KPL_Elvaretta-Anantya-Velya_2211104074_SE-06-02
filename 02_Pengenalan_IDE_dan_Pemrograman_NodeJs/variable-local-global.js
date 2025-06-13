c57f5e1df065e8844b26d3edeae7a97a1ca8dc2a
Epoysss <velyavanantya@gmail.com>
2025-06-13 22:11:08 +0700

    praktikum 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Elvaretta Anantya Velya"
+
+function getMyIdentity() {
+    let localVariable = 2211104074;
+
+    console.log(`Nama saya ${globalVarible} nim ${localVariable}`)
+}
+
+console.log(globalVarible)
+// console.log(localVariable)
+
+getMyIdentity()
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,16 @@
+var name = "Elvaretta Anantya Velya";
+const nim = 2211104074;
+
+console.log(name)
+
+console.log("nama saya adalah " + name + " dan nim saya adalah " + nim);
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`);
+
+
+var name = "ayam"
+
+let fullName = "Elvaretta Anantya Velya"
+
